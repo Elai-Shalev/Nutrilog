7edf7f07ada5cf10dfc50469133afb0652852f96
Elai Shalev <elaishalev@mail.tau.ac.il>
2023-06-16 15:11:40 +0300

    updated server to blob

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const axios = require('axios');
+const multer = require('multer');
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Create a router
 const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+
 
 // Define routes using the router
 router.get('/requestWeigh', (req, res) => {
@@ -12,14 +23,31 @@ router.get('/requestWeigh', (req, res) => {
 });
 
 router.post('/upload-photo', (req, res) => {
-  const { photo } = req.body;
+  try {
 
-  // Convert the base64 image to a Buffer
-  const imageData = Buffer.from(photo, 'base64');
+    const blob = req.file.buffer;
+    /*
+    const base64Data = req.body.image; // Assuming req.body.image contains the base64 string
+    const imageDatabuffer = Buffer.from(base64Data, 'base64');
+    const filename = 'testi.png';
+    fs.writeFileSync(filename, imageDatabuffer, 'base64');
+    */
+    const filename = 'new_from_blob.jpg'
+    fs.writeFile(filename, blob, (err) => {
+        if (err) {
+          console.error('Error saving the photo:', err);
+          res.status(500).json({ error: 'Failed to save the photo' });
+        } else {
+          console.log('Photo saved successfully');
+          res.json({ message: 'Photo uploaded and saved' });
+        }
+      });
 
-  console.log('Received image data:', imageData.length);
-
-  res.send(`Hello, client! This is a POST request. Received data: ${photo}`);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 // Mount the router at a specific base path
