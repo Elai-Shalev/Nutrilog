101be3e419be1b56b1c444bd80fbfbc63b16fb07
Elamar Barnea <ebarnea@nvidia.com>
2023-08-12 17:01:12 +0300

    form fillimg + resizing buttons to screen + handling options

--- NutrilogUI/App.js
+++ NutrilogUI/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import {
   Alert,
-  Button,
   StyleSheet,
   TouchableOpacity,
   View,
@@ -13,7 +12,7 @@ import { Camera } from "expo-camera";
 import CameraPreview from "./src/CameraPreview";
 import axios from "axios";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Button as Btn, Badge, ButtonGroup } from "react-native-elements";
+import { Button as Btn, ButtonGroup, Input } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
@@ -27,14 +26,42 @@ export default function App() {
   const [historyPageReq, setHistoryReq] = useState(false); //Request to history page
   const [NewMealRequest, setNewMealRequest] = useState(false); //Request to new meal
   const [FromDataOption, setFromDataOption] = useState(false); //Selecting from data and not from photo
-  const [WaitForOptions, setWaitForOptions] = useState(false);
-  const [ReceievedOptionsFromBack, setReceivedOptionsFromBack] = useState(false);
-  const [Option1FromPhotoAnalysis, setOption1FromPhotoAnalysis] = useState(1);
-  const [Option2FromPhotoAnalysis, setOption2FromPhotoAnalysis] = useState(2);
-  const [Option3FromPhotoAnalysis, setOption3FromPhotoAnalysis] = useState(3);
-  const [Option4FromPhotoAnalysis, setOption4FromPhotoAnalysis] = useState(4);
-  const [optionChosenFromList, setoptionChosenFromList] = useState(null);
+  const [WaitForOptions, setWaitForOptions] = useState(false); //Waiting to server to retreive options from db
+  const [ReceivedOptionsFromBack, setReceivedOptionsFromBack] = useState(false); //After Recieving Options
+  const [Option1FromPhotoAnalysis, setOption1FromPhotoAnalysis] = useState(1); // each option -1
+  const [Option2FromPhotoAnalysis, setOption2FromPhotoAnalysis] = useState(2); // each option -2
+  const [Option3FromPhotoAnalysis, setOption3FromPhotoAnalysis] = useState(3); // each option -3
+  const [Option4FromPhotoAnalysis, setOption4FromPhotoAnalysis] = useState(4); // each option -4
+  const [selectedIndex, setSelectedIndex] = useState(-1); // pressed button out of 4 options + other -> button index
+  const [fillMealFormSelected, setfillMealFormSelected] = useState(false); // if fifth button is pressed (other) - need to add manually the meal
+  const [inputValues, setinputValues] = useState({
+    name: "",
+    calories: "",
+    fat_total_g: "",
+    fat_saturated_g: "",
+    protein_g: "",
+    sodium_mg: "",
+    potassium_mg: "",
+    cholesterol_mg: "",
+    carbohydrates_total_g: "",
+    fiber_g: "",
+    sugar_g: "",
+  });
+  const iconsPerField = [
+    "tag",
+    "calculator",
+    "dashboard",
+    "dashboard",
+    "dashboard",
+    "dashboard",
+    "dashboard",
+    "dashboard",
+    "dashboard",
+    "dashboard",
+    "dashboard",
+  ]; // Define an array of icon names
   let camera;
+  const IPAddress = "172.20.10.5";
 
   //*****************************************************************************
   //*******************************Functions:************************************
@@ -67,13 +94,13 @@ export default function App() {
 
   //This is triggerd within the camera component inside the view
   const takePicture = async () => {
-    if (!camera) return
-    const options = { quality: 0.01};
+    if (!camera) return;
+    const options = { quality: 0.01 };
     const photo = await camera.takePictureAsync(options);
-    setSavedPhoto(photo)
-    setPreviewVisible(true)
-    setStartCamera(false)
-  }
+    setSavedPhoto(photo);
+    setPreviewVisible(true);
+    setStartCamera(false);
+  };
 
   //This is triggered within the clear picture button
   const clearPhoto = () => {
@@ -91,32 +118,34 @@ export default function App() {
 
     try {
       const photoData = new FormData();
-      photoData.append('photo', {
+      photoData.append("photo", {
         uri: savedPhoto.uri,
-        name: 'photo.jpg',
-        type: 'image/jpg',
+        name: "photo.jpg",
+        type: "image/jpg",
       });
-      const response = await axios.post('http://192.168.31.158:3000/api/upload', photoData);
-      console.log('Photo uploaded successfully:', response.data);
+      const response = await axios.post(
+        "http://192.168.31.158:3000/api/upload",
+        photoData
+      );
+      console.log("Photo uploaded successfully:", response.data);
       pollForResult();
     } catch (error) {
-      console.error('Error uploading photo:', error);
+      console.error("Error uploading photo:", error);
     }
-  }
+  };
 
-  
   async function pollForResult() {
     await axios
-      .get('http://192.168.31.158:3000/api/get-results')
-      .then(response => {
+      .get("http://192.168.31.158:3000/api/get-results")
+      .then((response) => {
         const data = response.data;
-        if (data.status === 'success') {
+        if (data.status === "success") {
           // Process the received result from the server
           console.log("received data");
           console.log(data.data);
           const topResults = data.data.results;
           console.log(topResults);
-  
+
           // Assuming these lines are related to handling the received data
           setOption1FromPhotoAnalysis(topResults[0].name);
           setOption2FromPhotoAnalysis(topResults[1].name);
@@ -124,50 +153,80 @@ export default function App() {
           setOption4FromPhotoAnalysis(topResults[3].name);
           setReceivedOptionsFromBack(true);
           setWaitForOptions(false);
-        } else if (data.status === 'processing') {
+        } else if (data.status === "processing") {
           console.log("still processing");
           // If still processing, continue polling
           setTimeout(pollForResult, 1000); // Poll every 1 second
         }
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
       });
   }
- 
-  //Send the user's option to the backend --complete
-   //inside- ask for the nutritional values of the food
 
-  //Receive the nutrition values from the backend 
-  async function getNutritionValuesFromServer(){
+  //Send the user's option to the backend --complete
+  //inside- ask for the nutritional values of the food
+
+  //Receive the nutrition values from the backend
+  async function getNutritionValuesFromServer() {
     try {
       console.log("enter getNutritionValues"); //check if can e removed --complete
-      const response = await axios.get('http://192.168.31.158:3000/api/get-NutritionValues');
+      const response = await axios.get(
+        "http://192.168.31.158:3000/api/get-NutritionValues"
+      );
       const nutrition_values = response.data.values;
 
-      console.log('The Nutrition Values are :', nutrition_values);
+      console.log("The Nutrition Values are :", nutrition_values);
     } catch (error) {
-      console.error('Error getting Nutrition Values:', error);
+      console.error("Error getting Nutrition Values:", error);
     }
   }
 
   //This function us triggered by "weigh now" button
   //It sends a request to weigh to the back end and gets the result and displays it
-  const getWeight = async() => {
-    setFoodCurrentlyWeighted(true)
-    try{
-      answer = await axios.get('http://192.168.31.158:3000/api/start-weigh');
-      weight = answer.data.weigh_val
-    }
-    catch (e){
-      console.log(e)
-      Alert.alert("couldn't connect to server")
-      BackHome()
+  const getWeight = async () => {
+    setFoodCurrentlyWeighted(true);
+    try {
+      answer = await axios.get("http://192.168.31.158:3000/api/start-weigh");
+      weight = answer.data.weigh_val;
+    } catch (e) {
+      console.log(e);
+      Alert.alert("couldn't connect to server");
+      BackHome();
     }
     setFoodCurrentlyWeighted(false);
     setDoneWeighting(true);
     // setSavedWeight(answer); --complete
     setSavedWeight(weight);
+  };
+
+  const sendChosenIndex = async (index) => {
+    console.log("hi", index);
+    if (index === 4) {
+      fillMealForm();
+    } else {
+      console.log("going to back");
+    }
+  };
+
+  const fillMealForm = () => {
+    setfillMealFormSelected(true);
+    setReceivedOptionsFromBack(false);
+  };
+
+  const handleInputChange = (inputName, value) => {
+    if (inputName != "name") {
+      if (/^\d+$/.test(value)) {
+        setinputValues((prevValues) => ({ ...prevValues, [inputName]: value }));
+      } else {
+        Alert.alert("Must be an Integer!");
+      }
+    }
+    setinputValues((prevValues) => ({ ...prevValues, [inputName]: value }));
+  };
+
+  const handleSubmit = () => {
+    console.log("Input Values:", inputValues);
   };
 
   const BackHome = () => {
@@ -195,7 +254,7 @@ export default function App() {
           !historyPageReq &&
           !NewMealRequest &&
           !FromDataOption && (
-            <View>
+            <View style={GeneralViewStyle}>
               <Header />
               <Btn
                 icon={<Icon name="plus" size={15} color="#f01f72" />}
@@ -216,11 +275,8 @@ export default function App() {
             </View>
           )}
         {NewMealRequest && !foodCurrentlyWeighted && !doneWeighting && (
-          <View>
-            <Text style={PictureSuccessfulDesign}>
-              The picture was saved successfully!
-            </Text>
-            <Text style={WeightRequestDesign}>Now please weigh your food</Text>
+          <View style={styles.container}>
+            <Text style={WeightRequestDesign}>Please weigh your food</Text>
             <Btn
               icon={<Icon name="forward" size={15} color="#f01f72" />}
               buttonStyle={buttonStyles.button}
@@ -254,7 +310,7 @@ export default function App() {
           !startCamera &&
           !savedPhoto &&
           !FromDataOption && (
-            <View>
+            <View style={styles.container}>
               <Text style={WeightReadyDesign}>
                 Your meal weight:{"\n"} {weight}
               </Text>
@@ -292,7 +348,7 @@ export default function App() {
           </View>
         )}
         {previewVisible && (
-          <View>
+          <View style={styles.container}>
             <CameraPreview photo={savedPhoto} />
             <Btn
               icon={<Icon name="camera" size={15} color="#f01f72" />}
@@ -372,16 +428,32 @@ export default function App() {
             </Text>
           </View>
         )}
-        {ReceievedOptionsFromBack && !WaitForOptions && (
-          <ButtonGroup
-            buttons={[
-              Option1FromPhotoAnalysis,
-              Option2FromPhotoAnalysis,
-              Option3FromPhotoAnalysis,
-              Option4FromPhotoAnalysis,
-              "Other",
-            ]}
-          />
+        {ReceivedOptionsFromBack && !WaitForOptions && (
+          <View>
+            <Text style={SelectOptionsDesign1}>
+              Please select an option that matches best.
+            </Text>
+            <Text style={SelectOptionsDesign2}>
+              If none of the options satisfies, please press other.
+            </Text>
+            <ButtonGroup
+              buttons={[
+                Option1FromPhotoAnalysis,
+                Option2FromPhotoAnalysis,
+                Option3FromPhotoAnalysis,
+                Option4FromPhotoAnalysis,
+                "Other",
+              ]}
+              selectedIndex={selectedIndex}
+              onPress={(value) => {
+                setSelectedIndex(value);
+                sendChosenIndex(value);
+              }}
+              containerStyle={buttonGroupStyles.container}
+              buttonStyle={buttonGroupStyles.button}
+              textStyle={buttonGroupStyles.text}
+            />
+          </View>
         )}
         {FromDataOption && (
           <Btn
@@ -392,6 +464,44 @@ export default function App() {
             color="#d13876"
             onPress={BackHome}
           />
+        )}
+        {fillMealFormSelected && (
+          <View style={GeneralViewStyle}>
+            <Text style={FormFillingDesign1}>New Meal:</Text>
+            <Text style={FormFillingDesign2}>
+              Please fill the fields per 100g. {"\n"}
+              Make sure it sums to this size and that{"\n"}
+              each field is in the right measurment unit.
+            </Text>
+            {Object.keys(inputValues).map((inputName, index) => (
+              <Input
+                key={inputName}
+                placeholder={
+                  "Enter " +
+                  inputName.charAt(0).toUpperCase() +
+                  inputName.slice(1)
+                }
+                value={inputValues[inputName]}
+                onChangeText={(value) => handleInputChange(inputName, value)}
+                containerStyle={stylesForSheetFill.container}
+                inputContainerStyle={{ width: "100%", borderBottomWidth: 0 }}
+                leftIcon={{
+                  type: "font-awesome",
+                  name: iconsPerField[index],
+                  size: 15,
+                  color: "pink",
+                }}
+              />
+            ))}
+            <Btn
+              icon={<Icon name="save" size={15} color="#f01f72" />}
+              buttonStyle={buttonStyles.button}
+              containerStyle={buttonStyles.container}
+              title="Submit"
+              color="#d13876"
+              onPress={handleSubmit}
+            />
+          </View>
         )}
       </View>
     </SafeAreaProvider>
@@ -412,6 +522,20 @@ const styles = StyleSheet.create({
   },
 });
 
+const SelectOptionsDesign1 = {
+  color: "#e06ca2",
+  textAlign: "center",
+  fontSize: 22,
+  fontWeight: 800,
+};
+
+const SelectOptionsDesign2 = {
+  color: "#ed1c93",
+  textAlign: "center",
+  fontSize: 15,
+  fontWeight: 700,
+};
+
 const ImageDesign = {
   flex: 1,
   justifyContent: "center",
@@ -423,13 +547,6 @@ const WeightRequestDesign = {
   textAlign: "center",
   fontSize: 35,
   fontWeight: 700,
-};
-
-const PictureSuccessfulDesign = {
-  color: "#bd0970",
-  textAlign: "center",
-  fontSize: 20,
-  fontWeight: 500,
 };
 
 const WeighWaitDesign1 = {
@@ -459,26 +576,87 @@ const WeighIsReadyDesign = {
   textAlign: "center",
 };
 
+const FormFillingDesign1 = {
+  color: "#fa4886",
+  textAlign: "center",
+  fontSize: 40,
+  fontWeight: 700,
+};
+
+const FormFillingDesign2 = {
+  color: "#fa4886",
+  textAlign: "center",
+  fontSize: 15,
+  fontWeight: 700,
+};
+
+const GeneralViewStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
 const buttonStyles = StyleSheet.create({
   button: {
     backgroundColor: "pink",
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 3,
+    marginRight: 0,
   },
   container: {
     width: 200,
-    marginHorizontal: 50,
     marginVertical: 10,
-    alignItems: "center",
+    alignContent: "center",
+    alignItems: "flex-start",
     justifyContent: "center",
-    display: "flex",
+    flexDirection: "row",
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
     fontWeight: "bold",
     letterSpacing: 0.25,
+    color: "black",
+  },
+});
+
+const buttonGroupStyles = StyleSheet.create({
+  button: {
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    width: 400,
+    marginHorizontal: 30,
+    marginVertical: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 17,
+    lineHeight: 35,
+    letterSpacing: 0.25,
+    color: "black",
+  },
+});
+
+const stylesForSheetFill = StyleSheet.create({
+  container: {
+    height: 40,
+    borderColor: "pink",
+    borderWidth: 1,
+    marginBottom: 10,
+    // paddingHorizontal: 150,
+    width: "62%",
+    fontSize: 15,
+  },
+  text: {
+    fontSize: 17,
+    lineHeight: 35,
+    letterSpacing: 0.25,
+    paddingHorizontal: 40,
     color: "black",
   },
 });
